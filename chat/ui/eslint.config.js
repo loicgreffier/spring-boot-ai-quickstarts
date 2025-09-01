637faf61b346549495a1664d6a58c61b57bc1ee1
Loïc Greffier <loic.greffier@michelin.com>
2025-09-01 19:23:47 +0200

    update

--- chat/ui/eslint.config.js
+++ chat/ui/eslint.config.js
@@ -2,42 +2,46 @@
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const simpleImportSort = require('eslint-plugin-simple-import-sort');
+const unusedImports = require('eslint-plugin-unused-imports');
 
 module.exports = tseslint.config(
   {
     files: ["**/*.ts"],
+		plugins: {
+			"simple-import-sort": simpleImportSort,
+			"unused-imports": unusedImports,
+		},
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
+			eslintPluginPrettierRecommended,
     ],
     processor: angular.processInlineTemplates,
     rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
-        {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
-        },
-      ],
-      "@angular-eslint/component-selector": [
-        "error",
-        {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
-        },
-      ],
-    },
+			"unused-imports/no-unused-imports": 'error',
+			"simple-import-sort/imports": "error",
+			"simple-import-sort/exports": "error",
+			"@typescript-eslint/no-unused-vars": [
+				"error",
+				{
+					args: "after-used",
+					argsIgnorePattern: "^ignored",
+					caughtErrors: "all",
+					caughtErrorsIgnorePattern: "^ignored"
+				}
+			]
+    }
   },
   {
     files: ["**/*.html"],
     extends: [
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
-    ],
-    rules: {},
+			eslintPluginPrettierRecommended
+    ]
   }
 );
