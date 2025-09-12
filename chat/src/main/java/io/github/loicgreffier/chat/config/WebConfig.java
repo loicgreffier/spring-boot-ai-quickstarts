843d034de8e239e22c15495977a05e344d0d21d0
Loïc Greffier <loic.greffier@michelin.com>
2025-09-12 23:58:07 +0200

    spotless

--- chat/src/main/java/io/github/loicgreffier/chat/config/WebConfig.java
+++ chat/src/main/java/io/github/loicgreffier/chat/config/WebConfig.java
@@ -22,6 +22,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -34,9 +36,6 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.io.IOException;
-import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -87,10 +86,9 @@ public class WebConfig implements WebMvcConfigurer {
         protected void doFilterInternal(
                 HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
                 throws ServletException, IOException {
-            String path = request.getRequestURI().substring(request.getContextPath().length());
-            if (!path.startsWith("/api")
-                    && !path.contains(".")
-                    && path.matches("/(.*)")) {
+            String path =
+                    request.getRequestURI().substring(request.getContextPath().length());
+            if (!path.startsWith("/api") && !path.contains(".") && path.matches("/(.*)")) {
                 request.getRequestDispatcher("/index.html").forward(request, response);
                 return;
             }
