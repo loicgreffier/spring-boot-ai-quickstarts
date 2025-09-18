20f2786e1f6e20bd38a129c6f4cc5beadba2630f
Loïc Greffier <loic.greffier@michelin.com>
2025-09-19 01:51:12 +0200

    Fix zone in frontend

--- chat/ui/src/app/app.config.ts
+++ chat/ui/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,7 +7,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideBrowserGlobalErrorListeners(),
-		provideZonelessChangeDetection(),
+		provideZoneChangeDetection({ eventCoalescing: true }),
 		provideRouter(routes),
 		provideHttpClient()
 	]
--- chat/ui/src/app/app.spec.ts
+++ chat/ui/src/app/app.spec.ts
@@ -1,6 +1,5 @@
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 
 import { App } from './app';
@@ -9,7 +8,7 @@ describe('App', () => {
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
 			imports: [App],
-			providers: [provideZonelessChangeDetection(), provideHttpClient(), provideHttpClientTesting()]
+			providers: [provideHttpClient(), provideHttpClientTesting()]
 		}).compileComponents();
 	});
 
